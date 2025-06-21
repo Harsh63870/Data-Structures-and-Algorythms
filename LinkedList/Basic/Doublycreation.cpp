85adfb2c5183ce42f71dee90ec25e3d8982d14ee
Harsh63870 <harshvardhanpandey372@gmail.com>
2025-06-21 10:33:40 +0530

    doubly added

---
+++ LinkedList/Basic/Doublycreation.cpp
@@ -0,0 +1,73 @@
+#include <iostream>
+
+using namespace std;
+// node creation 
+class Node{
+    public:
+        int data;
+        Node* next;
+        Node* prev;
+    public:
+        Node(int data , Node* next , Node* prev){
+            this->data = data;
+            this->prev = prev;
+            this->next = next;
+        }
+    public:
+        Node(int data){
+            this->data = data;
+            next = nullptr;
+            prev = nullptr;
+        }
+};
+// insertion of Node in the linked list 
+Node* insertAtbeg(Node* head , int data){
+    if(head == nullptr) return new Node(data);
+    Node* temp = new Node(data);
+    // Node* p = head;
+    temp->next = head;
+    head->prev = temp;
+    head = temp;
+    return head;
+}
+// forwardTraversal
+void traversal_Forward(Node* head){
+    if(head == nullptr) return;
+    Node* temp = head;
+    while(temp!=nullptr){
+        cout << temp->data << "->";
+        temp = temp->next;
+    }
+    cout << "nullptr" <<endl;
+}
+// backwardTraversal
+void traversal_Backword(Node* head){
+    if(head == nullptr){
+        return;
+    }
+    Node* temp = head;
+    while(temp->next!=nullptr){
+        // cout << temp->data << "->";
+        temp = temp->next;
+    }
+    Node* temp2 = temp;
+    cout<<"nullptr";
+    while(temp2 != NULL){
+        cout << "<-";
+        cout << temp2->data;
+        temp2 = temp2->prev;
+    }
+}
+int main(){
+    Node * head = new Node(22);
+    int n;
+    cin>>n;
+    for(int i=0 ; i<n ; i++){
+        int data;
+        cin>>data;
+        head = insertAtbeg(head , data);
+    }
+    traversal_Forward(head);
+    traversal_Backword(head);
+    return 0;
+}
